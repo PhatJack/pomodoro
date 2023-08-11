fd7cfb80cbeddf8c45be61586408b50fea20836e
PhatJack <tienphatng.693@gmail.com>
2023-08-11 19:33:33 +0700

    add background components

---
+++ src/components/Background.tsx
@@ -0,0 +1,66 @@
+import TokyoSakura from '../assets/TokyoSakura.png'
+import RainyDay from '../assets/RainyDay.png'
+import LifeGoesOn from '../assets/LifeGoesOn.png'
+import Galaxy from '../assets/Galaxy.png'
+import NeonCity from '../assets/NeonCity.png'
+import AestheticCafeteria from '../assets/AestheticCafeteria.png'
+import LightTrailsOnSuburbanHighway from '../assets/LightTrailsOnSuburbanHighway.png'
+
+
+const Background = () => {
+
+	const listImg = [
+		{
+			name: "Tokyo Sakura",
+			img: TokyoSakura,
+			value: "TokyoSakura"
+		},
+		{
+			name: "Aesthetic Cafeteria",
+			img: AestheticCafeteria,
+			value: "AestheticCafeteria"
+		},
+		{
+			name: "Neon City",
+			img: NeonCity,
+			value: "NeonCity"
+		},
+		{
+			name: "Galaxy",
+			img: Galaxy,
+			value: "Galaxy"
+		},
+		{
+			name: "Rainy Day",
+			img: RainyDay,
+			value: "RainyDay"
+		},
+		{
+			name: "Life Goes On",
+			img: LifeGoesOn,
+			value: "LifeGoesOn"
+		},
+		{
+			name: "Light Trails On Suburban Highway",
+			img: LightTrailsOnSuburbanHighway,
+			value: "LightTrailsOnSuburbanHighway"
+		}
+	]
+
+	const getImgSrc = listImg.filter((item) => { return localStorage.getItem("imageBg") == item.value.toString() })
+	console.log(getImgSrc[0].img)
+
+	return (
+		<div className="w-full h-full -z-10 fixed top-0 left-0 right-0 bottom-0 object-cover object-center">
+			<img
+				src={getImgSrc[0].img}
+				alt=""
+				className='w-full h-screen object-cover object-center -z-10 bg-black'
+				loading='lazy'
+				placeholder='TokyoSakura'
+			/>
+		</div>
+	)
+}
+
+export default Background
